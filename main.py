27857609c8be42774a8484af4cfb3828372e1275
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-22 19:51:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import HTMLParser from HTMLParser
+import Thread from threading
 
-class save(HTMLParser):
-    
+class Save(HTMLParser):
+    def handle_starttag():
+        
+
+def parse(self, saveList, url, hasParsedReg):
+    if url[:1] == "//":
+        url = "https://"
+    if url[:7] != "https://" && url[:6] != "http://":
+        raise Exception("Could not search invalid URI scheme")
+    self.
