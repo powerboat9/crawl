93bc042a0c27172f24804cd342de330805df8e6f
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-23 00:16:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,39 @@ import Thread from threading
 import re
 
 class Parse(HTMLParser):
-    def __init__(self, url, saveList, hasParsedList):
+    def __init__(self, url, saveList = (), unsafeList = (), hasParsedList = {}):
         self.saveList = saveList
         self.hasParsedList = hasParsedList
+        self.unsafeList = unsafeList
         self.baseURL = url
         self.url = url
         super(Parse, self).__init__()
+    def unsafe(self, url):
+        if !self.hasParsedList[url]:
+            self.unsafeList[self.unsafeList.length] = url
+            self.hasParsedList[url]
+    def share(self, url):
+        if !self.hasParsedList[url]:
+            self.saveList[self.saveList.length] = url
+            self.hasParsedList[url] = True
+            return True
+        return False
     def create(self, link):
-        newThread = Thread(target = threadMethod, args = (link, self.saveList, self.hasParsedList))
-        newthread
+        if self.share(link):
+            newThread = Thread(target = threadMethod, args = (link, self.saveList, self.hasParsedList))
+            newThread.daemon = True
+            newThread.start()
     def handle_starttag(self, tag, attributes):
         if tag == "a" || tag == "link":
             self.create(attributes.href)
-        elif tag == "script" || tag == "img" || :
+        elif tag == "script" || tag == "img" || tag == "audio" || tag == "embed":
+            self.share(attributes.src)
+        elif tag == "iframe" || tag == "source":
             self.create(attributes.src)
+        elif tag == "object":
+            self.create()
+        elif tag == "form":
+            self.unsafe(attributes.action)
 
 class UnderstandURL():
     def __init__(self, url)
