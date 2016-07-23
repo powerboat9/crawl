476ce62bb295ad61f71ad2f605c3cb699b705e07
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-22 20:14:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import HTMLParser from HTMLParser
 import Thread from threading
 
-class Save(HTMLParser):
-    def handle_starttag():
-        
+class Parse(HTMLParser):
+    def __init__(self, saveList, hasParsedList):
+        self.saveList = saveList
+        self.hasParsedList = hasParsedList
+        super(Parse, self).__init__()
+    def create(self, link):
+        newThread = Thread(target = threadMethod, args = (link, self.saveList, self.hasParsedList))
+        newthread
+    def handle_starttag(self, tag, attributes):
+        if tag == "a" || tag == "link":
+            self.create(attributes.href)
+        elif tag == "script" || tag == "img" || :
+            self.create(attributes.src)
 
-def parse(self, saveList, url, hasParsedReg):
+def threadMethod(url, saveList, hasParsedList):
     if url[:1] == "//":
         url = "https://"
     if url[:7] != "https://" && url[:6] != "http://":
         raise Exception("Could not search invalid URI scheme")
-    self.
+    Save
